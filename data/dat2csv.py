25ad2e02d41bc2cb3247f4568295b17347368120
Chris North <chris.north@astro.cf.ac.uk>
2016-06-22 11:21:15 +0100

    Added 3D plot

--- data/dat2csv.py
+++
@@ -1,7 +0,0 @@
-from astropy.table import Table
-
-table = Table.read('HATLAS_time_core.dat',format='ascii')
-
-table.write('HATLAS_time_core.csv',format='ascii.csv')
-
-
--- js/hatlas-3d.js
+++ js/hatlas-3d.js
@@ -2,32 +2,85 @@ function HATLASPlot(){
     return this;
 }
 
+HATLASPlot.prototype.rdz2xy = function (RAscl,Decscl,z) {
+    //convert from RA,DEC,z to isometric projects
+    // from https://en.wikipedia.org/wiki/Isometric_projection
+    x = Math.sqrt(1/2) * (RAscl - z)
+    y = Math.sqrt(1/6) * (RAscl + Decscl + z)
+    return [x,y];
+};
+HATLASPlot.prototype.scaleData = function(d){
+    //scale data
+    d.RAscl=0.5*(d.RA-this.dCtr.RA)/this.dRng.RA;
+    d.Decscl=(d.Dec-this.dCtr.Dec)/this.dRng.Dec;
+    d.zscl=-2*(d.z-this.dCtr.z)/this.dRng.z;
+    d.tscl=-2*(d.t-this.dCtr.t)/this.dRng.t;
+}
 HATLASPlot.prototype.drawGraphInit = function(){
-    console.log('reading data')
+    // console.log('reading data')
     var ha=this;
-
-    this.ang=10*Math.PI/180;
 
 
     d3.csv("data/HATLAS_time_core.csv", function(error, data){
-        console.log('data read')
+        // console.log('data read')
         ha.dataAll = data
         ha.dataAll.forEach(function(d){
             d.RA=+d.RA;
             d.Dec=+d.Dec;
             d.z=+d.z;
+            d.t=+d.LookbackTime;
         })
-        console.log('formatted');
+        // console.log('formatted');
         //calculate ranges
-        ha.dataRange = {
-            RAmin:d3.min(ha.dataAll,function(d){return d.RA}),
-            RAmax:d3.max(ha.dataAll,function(d){return d.RA}),
-            Decmin:d3.min(ha.dataAll,function(d){return d.Dec}),
-            Decmax:d3.max(ha.dataAll,function(d){return d.Dec}),
-            Zmin:d3.min(ha.dataAll,function(d){return d.z}),
-            Zmax:d3.max(ha.dataAll,function(d){return d.z})
+        ha.dMin={'t':0.0,'z':0};
+        ha.dMax={};
+        ha.dRng={};
+        ha.dCtr={};
+        // ha.forceMin={t:0}
+        var axes=['RA','Dec','z','t']
+        for (var a=0;a<axes.length;a++){
+            ax=axes[a];
+            // console.log(a,ax);
+            if (ha.dMin[ax]==null){ha.dMin[ax]=d3.min(ha.dataAll,function(d){return d[ax]});}
+            if (ha.dMax[ax]==null){ha.dMax[ax]=d3.max(ha.dataAll,function(d){return d[ax]});}
+            ha.dRng[ax]=ha.dMax[ax] - ha.dMin[ax];
+            ha.dCtr[ax]=ha.dMin[ax] + ha.dRng[ax]/2;
         }
-        console.log(this.dataRange);
+        ha.dataAll.forEach(function(d){
+            ha.scaleData(d);
+            [d.x,d.y]=ha.rdz2xy(d.RAscl,d.Decscl,d.tscl);
+        })
+        //calculate danges for
+        for (var a=0;a<axes.length;a++){
+            ax=axes[a]+'scl';
+            // console.log(a,ax);
+            ha.dMin[ax]=d3.min(ha.dataAll,function(d){return d[ax]});
+            ha.dMax[ax]=d3.max(ha.dataAll,function(d){return d[ax]});
+            ha.dRng[ax]=ha.dMax[ax] - ha.dMin[ax];
+            ha.dCtr[ax]=ha.dMin[ax] + ha.dRng[ax]/2;
+        }
+        // console.log(x,y);
+        var xy=['x','y']
+        for (var a=0;a<xy.length;a++){
+            ax=xy[a];
+            ha.dMin[ax]=d3.min(ha.dataAll,function(d){return d[ax]});;
+            ha.dMax[ax]=d3.max(ha.dataAll,function(d){return d[ax]});
+            ha.dRng[ax]=ha.dMax[ax] - ha.dMin[ax];
+            ha.dCtr[ax]=ha.dMin[ax] + ha.dRng[ax]/2;
+        }
+        // ha.dMin = {
+        //     RA:d3.min(ha.dataAll,function(d){return d.RA}),
+        //     Dec:d3.min(ha.dataAll,function(d){return d.Dec}),
+        //     Z:d3.min(ha.dataAll,function(d){return d.z}),
+        // }
+        // ha.dMax = {
+        //     RA:d3.max(ha.dataAll,function(d){return d.RA}),
+        //     Dec:d3.max(ha.dataAll,function(d){return d.Dec}),
+        //     Z:d3.max(ha.dataAll,function(d){return d.z})
+        // }
+        // ha.dataCtr = {
+        //     RA: 0.5*(ha.dataRange.RAmin + ha.dataRange.RAmin)
+        // }
         ha.scaleWindow();
         ha.makePlot()
     });
@@ -45,39 +98,43 @@ HATLASPlot.prototype.scaleWindow = function(){
     this.svgWidth = this.svgSize;
     this.margin={left:0,top:0,bottom:0,right:0}
 
-    this.xValue = function(d) {return d.RA;} // data -> value
+    // data -> value
+    this.xValue = function(d) {return d.x;}
     // value -> display
     this.xScale = d3.scale.linear()
-        .domain([this.dataRange.RAmin-0.1,this.dataRange.RAmax+0.1])
+        .domain([this.dMin.x-0.1,this.dMax.x+0.1])
         .range([0, this.svgWidth])
-        // data -> display
+    // data -> display
     this.xMap = function(d) { return ha.xScale(ha.xValue(d));}
-    this.yValue = function(d) {return d.Dec;} // data -> value
+
+    // data -> value
+    this.yValue = function(d) {return d.y;}
+    // value -> display
     this.yScale = d3.scale.linear()
-        .domain([this.dataRange.Decmin-0.1,this.dataRange.Decmax+0.1])
+        .domain([this.dMin.y-0.1,this.dMax.y+0.1])
         .range([0, this.svgHeight])
-        // data -> display
+    // data -> display
     this.yMap = function(d) { return ha.yScale(ha.yValue(d));}
 }
 
-HATLASPlot.prototype.filterZ = function(z1In,z2In){
+HATLASPlot.prototype.filterZ = function(z1In,z2In,ax){
+    if (ax==null){ax="z"}
     var z1 = (z1In) ? z1In : 0
     var z2 = (z2In) ? z2In : 0.01
     console.log(this.dataAll,z2);
     return this.dataAll.filter(function(d){
-        return ((d.z<=z2)&&(d.z>z1));
+        return ((d[ax]<=z2)&&(d[ax]>z1));
     })
 }
-
 HATLASPlot.prototype.makePlot = function(){
     var ha=this;
-    this.color=d3.scale.category20();
-    this.cValue = function(d) {return d.z;};
-    // this.color = d3.scale.linear()
-    //     .domain([-7,1])
-    //     .range(["red","blue"]);
-    // this.cValue = function(d) {return Math.log10(d.z);};
-    this.dotsize=2;
+    // this.color=d3.scale.category20();
+    // this.cValue = function(d) {return d.t;};
+    this.color = d3.scale.linear()
+         .domain([ha.dMin.t,ha.dMax.t])
+         .range(["#000000","#ff0000"]);
+    this.cValue = function(d) {return d.t;};
+    this.dotsize=1;
     svgcont = d3.select("div#graphcontainer").append("div")
         .attr("id","svg-container")
         .attr("width",this.svgWidth)
@@ -89,9 +146,9 @@ HATLASPlot.prototype.makePlot = function(){
     this.svg.append("g")
         .attr("transform", "translate(" + this.margin.left + "," +
             this.margin.top + ")")
-    this.dataFilt = this.filterZ();
-    this.dataFilt = this.dataAll;
-    console.log(this.dataFilt);
+    this.dataFilt = this.filterZ(ha.dMin.t,ha.dMax.t,'t');
+    // this.dataFilt = this.dataAll;
+    // console.log(this.dataFilt);
     this.svg.selectAll(".dot")
         .data(this.dataFilt)
         .enter()
@@ -99,8 +156,43 @@ HATLASPlot.prototype.makePlot = function(){
         .attr("r",this.dotsize)
         .attr("cx",ha.xMap)
         .attr("cy",ha.yMap)
-        .style("fill", function(d){return ha.color(ha.cValue(d));});
+        .style("fill", function(d){return ha.color(ha.cValue(d));})
+        .attr("opacity",0.5);
     // top line
+    var corners={
+        x0y0z0:ha.rdz2xy(ha.dMin.RAscl,ha.dMin.Decscl,ha.dMin.tscl),
+        x1y0z0:ha.rdz2xy(ha.dMax.RAscl,ha.dMin.Decscl,ha.dMin.tscl),
+        x0y1z0:ha.rdz2xy(ha.dMin.RAscl,ha.dMax.Decscl,ha.dMin.tscl),
+        x0y0z1:ha.rdz2xy(ha.dMin.RAscl,ha.dMin.Decscl,ha.dMax.tscl),
+        x1y1z0:ha.rdz2xy(ha.dMax.RAscl,ha.dMax.Decscl,ha.dMin.tscl),
+        x0y1z1:ha.rdz2xy(ha.dMin.RAscl,ha.dMax.Decscl,ha.dMax.tscl),
+        x1y0z1:ha.rdz2xy(ha.dMax.RAscl,ha.dMin.Decscl,ha.dMax.tscl),
+        x1y1z1:ha.rdz2xy(ha.dMax.RAscl,ha.dMax.Decscl,ha.dMax.tscl)
+    };
+    ax=['x','y','z'];
+    var lines={
+        x0y0:['x0y0z0','x0y0z1'],
+        x0y1:['x0y1z0','x0y1z1'],
+        x1y0:['x1y0z0','x1y0z1'],
+        x1y1:['x1y1z0','x1y1z1'],
+        x0z0:['x0y0z0','x0y1z0'],
+        x0z1:['x0y0z1','x0y1z1'],
+        x1z0:['x1y0z0','x1y1z0'],
+        x1z1:['x1y0z1','x1y1z1'],
+        y0z0:['x0y0z0','x1y0z0'],
+        y1z0:['x0y1z0','x1y1z0'],
+        y0z1:['x0y0z1','x1y0z1'],
+        y1z1:['x0y1z1','x1y1z1']
+    }
+    for (l in lines){
+        // console.log(l,lines[l][0],corners[lines[l][0]],corners[lines[l][0]][0],ha.xScale(corners[lines[l][0]][0]));
+        this.svg.append("line")
+            .attr("x1",ha.xScale(corners[lines[l][0]][0]))
+            .attr("x2",ha.xScale(corners[lines[l][1]][0]))
+            .attr("y1",ha.yScale(corners[lines[l][0]][1]))
+            .attr("y2",ha.yScale(corners[lines[l][1]][1]))
+            .style("stroke","black");
+    }
     this.svg.append("line")
         .attr("x1",0).attr("x2",ha.svgWidth)
         .attr("y1",0).attr("y2",0)
